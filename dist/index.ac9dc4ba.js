d018082ee4ef788e04410f2fd85d1dd356da91ac
CodeSandbox <support@codesandbox.io>
2023-12-07 10:10:28 +0000

    Initial commit

--- dist/index.ac9dc4ba.js
+++ dist/index.ac9dc4ba.js
@@ -577,7 +577,7 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"aR1JP":[function(require,module,exports) {
 var _stylesCss = require("./styles.css");
 document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
+<h1>Hola Mundo!</h1>
 `;
 
 },{"./styles.css":"lW6qc"}],"lW6qc":[function() {},{}]},["iZQ9B","aR1JP"], "aR1JP", "parcelRequirebaba")
--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,5 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
+<h1>Hola Mundo!</h1>
 `;
